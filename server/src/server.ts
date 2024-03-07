ca24372447f9cd4ce4b8823f846b2f17e6e0d432
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-03-06 21:40:46 -0500

    Add users route in server.ts file

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,6 +20,7 @@ app.get("/", (_, res) => res.sendStatus(200));
 
 app.use("/authors", (await import("./routes/authors.js")).default);
 app.use("/books", (await import("./routes/books.js")).default);
+app.use("/users", (await import("./routes/users.js")).default);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
