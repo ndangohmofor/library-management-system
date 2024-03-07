73aa34a54553cf055c369329543aa6f8f1829201
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-03-07 07:50:37 -0500

    added reviews route with method for getting all reviews for a book id

---
+++ server/src/routes/reviews.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { Request as AuthRequest } from "express-jwt";
+import { protectedRoute } from "../utils/middlewares";
+import BookController from "../controllers/books";
+import ReviewsController from "../controllers/reviews";
+
+//The router will be added as a middleware and will take control of requests starting with /books/:bookId/reviews.
+const reviews = Router({ mergeParams: true });
+export default reviews;
+
+const bookController = new BookController();
+const reviewsController = new ReviewsController();
+
+reviews.get("/", async (req: AuthRequest, res) => {
+  const bookId = req?.params?.bookId;
+
+  if (!bookId) {
+    return res
+      .status(400)
+      .send({ message: bookController.errors.BOOK_ID_MISSING });
+  }
+
+  try {
+    const reviews = await reviewsController.getReviews(bookId);
+    return res.json(reviews);
+  } catch (error) {
+    console.error(error.message);
+    return res
+      .status(500)
+      .send({ message: reviewsController.errors.UNKNOWN_ERROR });
+  }
+});
