186bdc788e8a6313194d0fa57d7be76534325d5e
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-03-17 23:48:08 -0400

    add book catalogue component

---
+++ client/src/app/book-catalogue/book-catalogue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-catalogue',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-catalogue.component.html',
+  styleUrl: './book-catalogue.component.css'
+})
+export class BookCatalogueComponent {
+
+}
